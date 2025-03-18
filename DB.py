32dc7e7495aa2fef95a50b1f6fc3e91387701db1
jaumegarcia <jaumegarcia@gmail.com>
2025-03-18 16:41:15 +0100

    adding degree

--- DB.py
+++ DB.py
@@ -1,8 +1,9 @@
 class DB:
-    def __init__(self, name, address, studies="physics"):
+    def __init__(self, name, address, studies="physics",degree="physics):
         self.name = name
         self.address = address
         self.studies = studies
+        self.degree = degree
 
     def __str__(self):
         return "Name:{}; Address:{}".format(self.name, self.address)
